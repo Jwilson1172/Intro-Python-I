53009f5ab639bcc29be600abbb3a52815bf9b4d8
JWilson1172 <jw59615@gmail.com>
2020-06-02 17:57:39 -0400

    finishing up a few problems

--- src/11_args.py
+++ src/11_args.py
@@ -1,8 +1,11 @@
+import numpy as np
+
 # Experiment with positional arguments, arbitrary arguments, and keyword
 # arguments.
 
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
+
 
 def f1(x: int, y: int) -> int:
     if isinstance(x, int) and isinstance(y, int):
@@ -11,33 +14,43 @@ def f1(x: int, y: int) -> int:
         raise ValueError("can only parse integers as arguments please try again")
         exit()
 
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-def f2( *nums: int) -> int:
-    return lambda x: x += x for x in nums
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+
+def f2(*args: int) -> int:
+    return sum(args)
+
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(a))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+
+def f2(*args: int) -> int:
+    if isinstance(args[0], int):
+        return sum(args)
+    elif isinstance(args[0], list):
+        return sum(np.array(args[0]))
+
 
 print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+print(f3(8))  # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -48,7 +61,11 @@ print(f3(8))     # Should print 9
 #
 # Note: Google "python keyword arguments".
 
-# YOUR CODE HERE
+
+def f4(**kwargs):
+    [print(f"key: {k}, value: {v}") for k, v in zip(kwargs.keys(), kwargs.values())]
+    return
+
 
 # Should print
 # key: a, value: 12
@@ -61,10 +78,7 @@ f4(a=12, b=30)
 # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+d = {"monster": "goblin", "hp": 3}
 
 # How do you have to modify the f4 call below to make this work?
 f4(d)
